6802fdc6cc7c7573ee545a05bdb643b0d1ab5f97
Nadia Odunayo <nodunayo@gmail.com>
2015-10-03 13:20:46 +0100

    Update install_brews.sh and install_casks.sh

--- install_brews.sh
+++ install_brews.sh
@@ -1,1 +1,15 @@
-echo "Installing Homebrew packages..."
+#!/usr/bin/env sh
+
+formulae=(
+  "git"
+  "chruby"
+  "postgresql"
+  "ruby-build"
+  "tmux"
+)
+
+for formula in ${formulae[@]}
+do
+  echo "Installing: ${formula}..."
+  brew install $formula
+done
--- install_casks.sh
+++ install_casks.sh
@@ -1,1 +1,25 @@
-echo "Installing Homebrew Casks..."
+#!/usr/bin/env sh
+
+casks=(
+  "1password"
+  "caffeine"
+  "dash"
+  "dropbox"
+  "evernote"
+  "firefox"
+  "flux"
+  "flycut"
+  "google-chrome"
+  "iterm2"
+  "s3cmd"
+  "skype"
+  "slack"
+  "things"
+  "vlc"
+)
+
+for cask in ${casks[@]}
+do
+  echo "Installing: ${cask}"
+  brew cask install $cask
+done
